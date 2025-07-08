5e9c2575dc15997bb50e4a43ec670e0c29a2b4e9
vishnujeet0609 <vishnujeet0609@gmail.com>
2025-07-08 12:17:54 +0530

    initial commit

---
+++ src/main/java/com/example/EcommerceSpring/EcommerceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringApplication.class, args);
+	}
+
+}
