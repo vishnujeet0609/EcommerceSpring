09488dce5a3929e7690467c8a2a9116d5ad78344
vishnujeet0609 <vishnujeet0609@gmail.com>
2025-08-03 00:57:09 +0530

    Added Dotenv step for basic .env

--- src/main/java/com/example/EcommerceSpring/EcommerceSpringApplication.java
+++ src/main/java/com/example/EcommerceSpring/EcommerceSpringApplication.java
@@ -1,5 +1,6 @@
 package com.example.EcommerceSpring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceSpringApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load(); //load environment variables from .env file
+		dotenv.entries().forEach(entry->System.setProperty(entry.getKey(),entry.getValue())); //get system properties fom environment variables and set them
+
 		SpringApplication.run(EcommerceSpringApplication.class, args);
+
 	}
 
 }
